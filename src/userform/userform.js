fa4fd271745558027eae654661bbbcd0b32e660b
Aishwarya Srivastava <aishwaryaaaaaaaaaaa@gmail.com>
2021-04-22 11:21:47 +0530

    first commit

---
+++ src/userform/userform.js
@@ -0,0 +1,21 @@
+import{useState} from "react";
+
+function Userform(){ //component,controller
+    //Model
+    const[userform,setUserform]=useState({
+    name:'Ram'
+    })
+    return(//view
+        <div>
+            <input value={userform.name} onChange={handleEvent}></input>
+        </div>
+    )
+   function  handleEvent(event){
+    console.log(event);
+    setUserform({
+    name:event.target.value
+})
+//name=event.target.value;
+    }
+}
+export default Userform;
